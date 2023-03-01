ce395959f91f567a5c2c4cd9f12ca189d4bf5b1a
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2023-03-01 00:05:31 -0500

    [FlashpointInstaller] Add minimize button to main window

--- FlashpointInstaller/src/Forms/Main.Designer.cs
+++ FlashpointInstaller/src/Forms/Main.Designer.cs
@@ -250,7 +250,6 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MinimizeBox = false;
             this.Name = "Main";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Flashpoint Installer";
